344a07d38ac3d0ad496f986e55d40bedaab9c0a7
Marek Szuba <mszuba01@gmail.com>
2020-12-23 20:25:53 +0100

    add search panel with suggestion bar

--- frontend/src/components/Common/SearchOptions/SearchOptionsElements.js
+++
@@ -1,3 +0,0 @@
-import styled from 'styled-components';
-
-export const SearchOptionsContainer = styled.div``;
--- frontend/src/components/Common/SearchPanel/SearchPanelElements.js
+++ frontend/src/components/Common/SearchPanel/SearchPanelElements.js
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 import { FiSearch } from 'react-icons/fi';
 
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
+
 export const SearchPanelContainer = styled.div`
   height: 100%;
   width: 25%;
@@ -18,7 +27,7 @@ export const SearchForm = styled.form`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: center;
   /* display: block; */
 `;
@@ -37,8 +46,8 @@ export const IconSearch = styled(FiSearch)`
   font-size: 26px;
 `;
 
-export const Input = styled.input`
-  width: 70%;
+export const Input = styled(ComboboxInput)`
+  width: 90%;
   font-size: 13px;
   border: none;
   color: #a4a5a6;
@@ -49,5 +58,50 @@ export const Input = styled.input`
   &::placeholder {
     color: #a4a5a6;
     opacity: 1;
+  }
+`;
+
+export const MyCombobox = styled(Combobox)`
+  width: 70%;
+  display: flex;
+  align-items: center;
+`;
+
+export const MyComboboxPopover = styled(ComboboxPopover)`
+  position: absolute;
+  top: 70px;
+  right: -1px;
+  width: 75%;
+  max-height: 275px;
+
+  padding: 30px 0 15px 0;
+
+  white-space: nowrap;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  overflow-y: scroll;
+
+  background-color: #fff;
+  border: 1px solid #e8ebf2;
+  border-top: none;
+  box-shadow: -10px 9px 21px rgba(128, 152, 213, 0.07);
+`;
+
+export const MyComboboxList = styled(ComboboxList)`
+  margin: 0;
+  padding-top: 10px;
+  list-style: none;
+
+  user-select: none;
+`;
+
+export const MyComboboxOption = styled(ComboboxOption)`
+  margin: 0;
+  padding: 8px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f6f7fa;
+    color: #8954ba;
   }
 `;
