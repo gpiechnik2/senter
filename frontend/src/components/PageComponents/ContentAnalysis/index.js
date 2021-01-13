e4aea9e0e3eae87ee86a5278fe75eb48e9370543
Marek Szuba <mszuba01@gmail.com>
2021-01-13 21:32:16 +0100

    migrate Data from component

--- frontend/src/components/PageComponents/ContentAnalysis/index.js
+++ frontend/src/components/PageComponents/ContentAnalysis/index.js
@@ -26,24 +26,9 @@ import {
 
 import { ButtonBasic } from '../../Common/ButtonElements';
 
+import { analysisExample } from './Data';
+
 const ContentAnalysis = () => {
-  const analysisExample = [
-    {
-      message:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting indusstry. ',
-      status: 'Valid',
-    },
-    {
-      message:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting indsustry. ',
-      status: 'Invalid',
-    },
-    {
-      message:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting iandustry. ',
-      status: 'Undefined',
-    },
-  ];
   return (
     <>
       <ContentAnalysisContainer>
