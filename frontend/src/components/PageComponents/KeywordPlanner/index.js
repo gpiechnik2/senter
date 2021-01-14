8fdb647f3b4cbe0e272a9ae0ea48e869b05f7908
Marek Szuba <mszuba01@gmail.com>
2021-01-14 14:22:40 +0100

    update data and keywordPlanner subpage

--- frontend/src/components/PageComponents/KeywordPlanner/index.js
+++ frontend/src/components/PageComponents/KeywordPlanner/index.js
@@ -1,5 +1,13 @@
 import Select from 'react-select';
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
 import { KeywordPlannerContainer } from './KeywordPlannerElements';
 
 import {
@@ -14,11 +22,19 @@ import {
 
 import { ButtonBasic } from '../../Common/ButtonElements';
 
+import { ColumnContainerBasic } from '../../Common/ContainerElements';
+
+import {
+  ExpandableAnalysisContainer,
+  AnalysisElementWrapper,
+  AnalysisElement,
+  ElementTitle,
+  ElementText,
+} from '../../Common/AnalysisElements';
+
+import { options1, analysisContent } from './Data';
+
 const KeywordPlanner = () => {
-  const options1 = [
-    { label: 'Polish', value: 'polish' },
-    { label: 'English', value: 'english' },
-  ];
   return (
     <>
       <KeywordPlannerContainer>
@@ -52,6 +68,43 @@ const KeywordPlanner = () => {
             </FormBtnWrap>
           </FormWrap>
         </FormContainer>
+        {analysisContent.map((analysis) => (
+          <ColumnContainerBasic key={analysis.title}>
+            <ExpandableAnalysisContainer>
+              <Accordion allowMultipleExpanded={true} allowZeroExpanded={true}>
+                <AccordionItem>
+                  <AccordionItemHeading>
+                    <AccordionItemButton className='accordion__button--analysis'>
+                      {analysis.title}
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel className='accordion__panel--analysis'>
+                    <AnalysisElementWrapper>
+                      <AnalysisElement>
+                        <ElementTitle>
+                          {analysis.label1 ? analysis.label1 : null}{' '}
+                        </ElementTitle>
+                        <ElementText>{analysis.text1}</ElementText>
+                      </AnalysisElement>
+                      <AnalysisElement>
+                        <ElementTitle>{analysis.label2} </ElementTitle>
+                        <ElementText>{analysis.text2}</ElementText>
+                      </AnalysisElement>
+                      <AnalysisElement>
+                        <ElementTitle>{analysis.label3} </ElementTitle>
+                        <ElementText>{analysis.text3}</ElementText>
+                      </AnalysisElement>
+                      <AnalysisElement>
+                        <ElementTitle>{analysis.label4} </ElementTitle>
+                        <ElementText>{analysis.text4}</ElementText>
+                      </AnalysisElement>
+                    </AnalysisElementWrapper>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              </Accordion>
+            </ExpandableAnalysisContainer>
+          </ColumnContainerBasic>
+        ))}
       </KeywordPlannerContainer>
     </>
   );
