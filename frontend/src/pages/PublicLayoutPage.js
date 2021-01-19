e23e996a902d315bec111dbf671f00e07c181e89
Marek Szuba <mszuba01@gmail.com>
2021-01-19 19:31:21 +0100

    add message actions

--- frontend/src/pages/PublicLayoutPage.js
+++ frontend/src/pages/PublicLayoutPage.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import LoginPage from './login';
 import RegisterPage from './register';
 
+import { Toaster } from 'react-hot-toast';
+
 import PublicLayout from '../components/Layouts/PublicLayout/index';
 import Header from '../components/Common/Header';
 import CornerMenu from '../components/Common/CornerMenu';
@@ -15,6 +17,18 @@ const PublicLayoutPage = () => {
         <Header></Header>
         <CornerMenu isPrivate={0} />
         <ContentContainer>
+          <Toaster
+            toastOptions={{
+              className: '',
+              style: {
+                margin: '80px',
+
+                background: '#5D38DB',
+                padding: '16px',
+                color: '#fff',
+              },
+            }}
+          />
           <Switch>
             <Route path='/login' component={LoginPage} />
             <Route path='/register' component={RegisterPage} />
