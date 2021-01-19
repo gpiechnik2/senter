6927874209eebe962c873ed9721a496536091ea0
Marek Szuba <mszuba01@gmail.com>
2021-01-19 23:28:42 +0100

    add request headers to axios for backend auth

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,5 +2,17 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:8000' });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`;
+  }
+
+  return req;
+});
+
 export const signUp = (formData) => API.post('/auth/users/', formData);
 export const signIn = (formData) => API.post('/auth/token/login/', formData);
+export const signGoogle = (token) =>
+  API.post('/auth/google/token/login/', token);
