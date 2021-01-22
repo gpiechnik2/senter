bf2eb24b8f9e2d48d284a8d49353d4233c458a12
Marek Szuba <mszuba01@gmail.com>
2021-01-22 19:08:10 +0100

    fix req.headers.auth with the right variable

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,7 +5,7 @@ const API = axios.create({ baseURL: 'http://localhost:8000' });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Token ${
-      JSON.parse(localStorage.getItem('profile')).token
+      JSON.parse(localStorage.getItem('profile')).auth_token
     }`;
   }
 
@@ -16,3 +16,6 @@ export const signUp = (formData) => API.post('/auth/users/', formData);
 export const signIn = (formData) => API.post('/auth/token/login/', formData);
 export const signGoogle = (tokenGoogle) =>
   API.post('/auth/google/token/login/', tokenGoogle);
+
+export const createKeyword = (formData) =>
+  API.post('/keyword-planner/', formData);
