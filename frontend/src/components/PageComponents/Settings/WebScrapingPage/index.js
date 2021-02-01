91203202b062a21407c0ec1e9fa3acad5cd6d71a
Marek Szuba <mszuba01@gmail.com>
2021-02-01 01:06:11 +0100

    update webScraping component for changeUserAgent

--- frontend/src/components/PageComponents/Settings/WebScrapingPage/index.js
+++ frontend/src/components/PageComponents/Settings/WebScrapingPage/index.js
@@ -1,5 +1,11 @@
-import { WebScrapingPageContainer } from './WebScrapingPageElements';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { changeuseragent } from '../../../../actions/changeUserAgent';
+
+import toast from 'react-hot-toast';
+
+import { WebScrapingPageContainer } from './WebScrapingPageElements';
 import {
   FormContainer,
   FormTitle,
@@ -10,20 +16,70 @@ import {
 } from '../../../Common/FormElements';
 import { ButtonBasic } from '../../../Common/ButtonElements';
 
+const initialState = {
+  user_agent: '',
+};
+
+let changeToast;
+
 const WebScrapingPage = () => {
+  const { userAgentChangeData, isLoading, isError } = useSelector(
+    (state) => state.changeUserAgentReducer
+  );
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(changeuseragent(formData));
+    setFormData({ ...formData, user_agent: '' });
+
+    changeToast = toast.loading('Changing user agent...');
+  };
+
+  useEffect(() => {
+    if (userAgentChangeData?.user_agent) {
+      toast.success(userAgentChangeData.user_agent, {
+        id: changeToast,
+      });
+    } else if (userAgentChangeData?.User && isError) {
+      toast.error(userAgentChangeData.User, {
+        id: changeToast,
+      });
+    } else if (isError) {
+      toast.error('Unable to change user agent.', {
+        id: changeToast,
+      });
+    }
+  }, [userAgentChangeData, isError]);
+
   return (
     <>
       <WebScrapingPageContainer>
         <FormContainer>
-          <FormTitle>Headers</FormTitle>
+          <FormTitle>User agent</FormTitle>
           <FormText>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy.
           </FormText>
-          <FormWrap>
-            <FormInput placeholder='Mozilla/43s/320 - 32 cddd' />
+          <FormWrap onSubmit={onSubmit}>
+            <FormInput
+              required
+              type='text'
+              name='user_agent'
+              placeholder='Mozilla/43s/320 - 32 cddd'
+              aria-describedby='Enter user agent'
+              value={formData.user_agent}
+              onChange={handleChange}
+            />
             <FormBtnWrap>
-              <ButtonBasic>Change</ButtonBasic>
+              <ButtonBasic type='submit' disabled={isLoading}>
+                Change
+              </ButtonBasic>
             </FormBtnWrap>
           </FormWrap>
         </FormContainer>
