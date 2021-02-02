a5381519d720d830759b3f1a43c3f480910b4e33
Marek Szuba <mszuba01@gmail.com>
2021-02-02 19:17:11 +0100

    add about_us page

---
+++ frontend/src/pages/about-us.js
@@ -0,0 +1,11 @@
+import AboutUs from '../components/PageComponents/AboutUs';
+
+const AboutUsPage = () => {
+  return (
+    <>
+      <AboutUs />
+    </>
+  );
+};
+
+export default AboutUsPage;
--- frontend/src/pages/blog.js
+++
@@ -1,9 +0,0 @@
-const BlogPage = () => {
-  return (
-    <>
-      <h1>BLOG PAGE</h1>
-    </>
-  );
-};
-
-export default BlogPage;
